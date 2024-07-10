f7e101765f9e8eca47b1f97cec6b8076d390246a
Fratiman <bogdanfrman@yahoo.com>
2024-07-10 17:27:37 +0300

    Done header -> Started front-page

---
+++ front-page.php
@@ -0,0 +1,72 @@
+<?php get_header(); ?>
+
+<body class="bg-gray-100">
+<!-- Hero Section -->
+<div class="relative bg-cover bg-center h-96" style="background-image: url('https://gegi.ro/wp-content/uploads/2015/12/prima-pagina-e1450789166952.png');">
+    <div class="absolute inset-0 bg-black opacity-50"></div>
+    <div class="absolute inset-0 flex items-center justify-center">
+        <div class="text-center text-white">
+            <h1 class="text-6xl font-semibold">Welcome to my site!</h1>
+            <p class="font-light text-3xl mt-5">The land of opportunity.</p>
+            <a class="px-5 py-2 inline-block bg-cyan-500 text-white hover:bg-cyan-400 transition-colors mt-10" href="">
+                Get Started
+            </a>
+        </div>
+    </div>
+</div>
+
+<div class="container mx-auto p-6" style="padding-top: 100px;">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
+        <div class="col-span-1">
+            <a href="http://gegi.ro/inchiriere-autocare/">
+                <div class="bg-white shadow-md rounded p-6 text-center transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
+                    <img src="http://gegi.ro/wp-content/uploads/2015/11/inchiriere-alb-150x150.png" alt="Inchiriere Autocare" class="w-24 h-24 mx-auto mb-4">
+                    <h4 class="text-lg font-bold text-blue-500 mb-2">Inchiriere Autocare</h4>
+                    <p>Te ajutam sa iti creezi traseele tale personalizate. Suntem langa tine oriunde, oricand pentru siguranta totala. Foarte multe solutii si servicii te asteapta, completate bineinteles de confort.</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-span-1">
+            <a href="http://transfero.ro">
+                <div class="bg-white shadow-md rounded p-6 text-center transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
+                    <img src="http://gegi.ro/wp-content/uploads/2015/06/avion-alb-150x150.png" alt="Transfer Aeroport" class="w-24 h-24 mx-auto mb-4">
+                    <h4 class="text-lg font-bold text-yellow-500 mb-2">Transfer Aeroport</h4>
+                    <p>Conexiune rapida cu Aeroportul International Henri Coanda – Otopeni. Rezerva-ti locurile sau vizualizeaza tarifele.</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-span-1">
+            <a href="http://gegi.ro/turism/">
+                <div class="bg-white shadow-md rounded p-6 text-center transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
+                    <img src="http://gegi.ro/wp-content/uploads/2015/11/turn-alb.png" alt="Turism" class="w-24 h-24 mx-auto mb-4">
+                    <h4 class="text-lg font-bold text-red-500 mb-2">Turism</h4>
+                    <p>Alaturi de partenerii nostri iti oferim cele mai frumoase vacante la cele mai avantajoase preturi.</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-span-1">
+            <a href="http://gegi.ro/grafice-de-circulatie/">
+                <div class="bg-white shadow-md rounded p-6 text-center transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
+                    <img src="http://gegi.ro/wp-content/uploads/2015/11/curse-alb-150x150.png" alt="Trasee si Grafice" class="w-24 h-24 mx-auto mb-4">
+                    <h4 class="text-lg font-bold text-green-500 mb-2">Trasee si Grafice</h4>
+                    <p>Informeaza-te cu ultimile modificari ale programelor si rutelor efectuate de masinile noastre. Vizualizeaza aceasta sectiune pentru a cunoaste oricare deviatie sau schimbare de rute.</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-span-1">
+            <a href="http://gegi.ro/contact/">
+                <div class="bg-white shadow-md rounded p-6 text-center transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
+                    <img src="http://gegi.ro/wp-content/uploads/2015/11/solutions-alb.png" alt="Alte Servicii" class="w-24 h-24 mx-auto mb-4">
+                    <h4 class="text-lg font-bold text-purple-500 mb-2">Alte Servicii</h4>
+                    <p>Vezi alte servicii oferite de GEGI Turism.</p>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
