efb20358f718dad98cddb6ee3aa094a2c953371d
Fratiman <bogdanfrman@yahoo.com>
2024-07-13 15:11:47 +0300

    Implemented Bus without rent.

--- page-bus.php
+++ page-bus.php
@@ -1,117 +1,29 @@
 <?php /* Template Name: Bus */
 get_header(); ?>
 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<link rel="stylesheet" href="magnific-popup.css">
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+<script src="js/jquery.magnific-popup.js"></script>
+
 <style>
-/* Existing CSS styles */
-.large-banner {
-    position: relative;
-    overflow: hidden;
-}
-
-.bg_img {
-    width: 100%;
-    height: auto;
-}
-
-.banner-info {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+.contact-info {
     text-align: center;
-    color: #fff;
 }
 
-.breadcrumbs {
-    background: #f5f5f5;
-    padding: 10px 0;
-}
-
-.bus {
-    padding: 30px 0;
-}
-
-.bus_img {
-    width: 100%;
-    height: auto;
-    margin-bottom: 20px;
-}
-
-.page-title {
-    font-size: 2.5em;
-    margin-bottom: 20px;
-}
-
-.call-center {
-    text-align: right;
-}
-
-.call-center .icon {
-    display: inline-block;
-    margin-right: 10px;
-}
-
-.buses .col-bus {
-    margin-bottom: 30px;
-}
-
-.bus-row {
+.contact-info a {
     display: flex;
-    border: 1px solid #e1e1e1;
-    padding: 20px;
-    border-radius: 10px;
-    background: #fff;
-}
-
-.bus-image {
-    flex: 1;
-    margin-right: 20px;
-}
-
-.car-details {
-    flex: 2;
-}
-
-.seats-availability {
-    flex: 1;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    max-width: 200px;
     text-align: center;
 }
 
-.seats-availability .button {
-    display: block;
-    margin-top: 20px;
-    background: #0073e6;
-    color: #fff;
-    padding: 10px;
-    border-radius: 5px;
-    text-decoration: none;
-    cursor: pointer;
-}
-
-.form-rent-bus-container {
-    display: none;
-    margin-top: 20px;
-}
-
-.bus-info-col {
-    background: #f9f9f9;
-    padding: 20px;
-    border-radius: 10px;
-}
-
-.container-form-bus {
-    margin-top: 20px;
-}
-
-.homepage-info-text {
+.rent {
     text-align: center;
-    margin-top: 40px;
 }
 
-/* New CSS styles */
-.form-rent-bus-container.active {
-    display: block;
-}
 </style>
 
 <!-- Hero Section -->
@@ -119,8 +31,9 @@ get_header(); ?>
     <div class="absolute inset-0 bg-black opacity-50"></div>
     <div class="absolute inset-0 flex items-center justify-center">
         <div class="text-center text-white">
-            <h1 class="text-6xl font-semibold"><?php _e( 'Inchiriere autocare' ) ?></h1>
-            <p class="font-light text-3xl mt-5"><?php the_field( 'descriere_hero_bus' ); ?></p>
+            <h1 class="text-4xl lg:text-6xl font-semibold"><?php _e( 'Autocare si microbuze premium.' ) ?></h1>
+            <p class="font-light text-lg lg:text-3xl mt-5"><?php _e( 'Servicii de transport la standarde inalte de confort.' ) ?></p>
+            <p class="font-light text-lg lg:text-3xl mt-5"><?php the_field( 'descriere_hero_bus' ); ?></p>
             <div class="flex justify-center mt-10">
                 <a href="#buses" style="color: #FFFFFF" class="flex items-center px-4 py-2 font-medium text-white bg-blue-900 rounded-md hover:bg-blue-800">VEZI OFERTA</a>
             </div>
@@ -128,138 +41,142 @@ get_header(); ?>
     </div>
 </div>
 
-<div class="breadcrumbs">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <?php get_template_part( 'layouts/breadcrumbs' ) ?>
+<div class="bus py-8">
+    <div class="container mx-auto">
+        <div class="flex flex-wrap">
+            <div class="w-full lg:w-2/3" id="buses">
+                <h1 class="text-4xl font-bold mb-8 rent">Inchiriere autocare</h1>
             </div>
-        </div>
-    </div>
-</div>
+            <div class="w-full lg:w-1/3">
+                <div class="text-right contact-info">
+                    <div class="mb-4 text-gray-800">Call center inchirieri:</div>
+                    <a href="tel:+40742401000" class="text-blue-600 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2 flex-shrink-0 mt-1">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                        </svg>
+                        <span class="inline-block align-middle">+4 0742 401 000</span>
+                    </a>
 
-<div class="bus" id="buses">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-7">
-                <h1 class="page-title"><?php _e( 'Inchiriere autocare' ) ?></h1>
-            </div>
-            <div class="col-sm-5">
-                <div class="call-center">
-                    <span class="icon"><svg width="19" height="19" viewBox="0 0 19 19"><use xlink:href="#phone-icon"></use></svg></span>
-                    <?php _e( 'Call center inchirieri' ) ?>
-                    <a class="call" href="tel:<?php echo preg_replace( '/\s+/', '', get_field( 'telefon_call_center_autocare', 'option' ) ); ?>"><?php the_field( 'telefon_call_center_autocare', 'option' ) ?></a>
-                    <a class="call" href="mailto:<?php echo get_field( 'email_rezervari_autocare', 'option' ); ?>"><?php the_field( 'email_rezervari_autocare', 'option' ) ?></a>
+                    <div class="mt-2">
+                        <a href="mailto:rentabus@transfero.ro" class="text-blue-600">rentabus@transfero.ro</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="buses">
-        <div class="container">
-            <?php
-            $args = array(
-                'post_type'      => 'bus',
-                'posts_per_page' => - 1,
-                'orderby'        => 'menu_order',
-                'order'          => 'ASC',
-            );
-            $loop = new WP_Query( $args );
-            if ( $loop->have_posts() ) : ?>
-                <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+</div>
 
-                    <div class="col-bus">
-                        <div class="bus-row">
-                            <div class="bus-image">
-                                <a class="gallery-link first-picture" href="<?php the_field( 'bus_imagine' ) ?>">
-                                    <img src="<?php the_field( 'bus_imagine' ) ?>" alt="" class="gallery-link-mg">
-                                </a>
-                                <?php
-                                $images = get_field( 'gelerie_bus', get_the_ID() );
-                                if ( $images ): ?>
-                                    <?php foreach ( $images as $image ): ?>
-                                        <a class="gallery-link" href="<?php echo $image['url']; ?>">
-                                            <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>">
-                                        </a>
-                                    <?php endforeach; ?>
-                                <?php endif; ?>
-                            </div>
-                            <div class="car-details">
-                                <h2 class="title"><?php echo get_the_title() ?></h2>
-                                <span class="options"><?php _e('Dotari interioare')?></span>
-                                <ul class="options-list">
-                                    <?php if ( have_rows( 'dotari_interioare' ) ):
-                                        while ( have_rows( 'dotari_interioare' ) ) : the_row(); ?>
-                                            <li><?php the_sub_field( 'dotare' ); ?></li>
-                                        <?php endwhile; endif; ?>
-                                </ul>
-                            </div>
-                            <div class="seats-availability">
-                                <div class="seats">
-                                    <span><strong><?php the_field( 'nr_locuri' ) ?></strong> <?php _e('locuri')?></span>
-                                </div>
-                                <div class="availability"><?php _e('Disponibilitate')?>: <?php the_field( 'disponibilitate' ) ?> <?php if( get_field('disponibilitate') == 1) {echo __('masina');}else{ echo __('masini');}?></div>
-                                <a href="#" class="button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" data-bus="<?php echo 'bus-' . get_the_ID() ?>"><span class="close">Solicita oferta</span></a>
-                            </div>
+<div class="buses container mx-auto py-8">
+    <div class="grid grid-cols-1 gap-6">
+        <?php
+        $args = array(
+            'post_type'      => 'bus',
+            'posts_per_page' => -1,
+            'orderby'        => 'menu_order',
+            'order'          => 'ASC',
+        );
+        $loop = new WP_Query( $args );
+        if ( $loop->have_posts() ) :
+            while ( $loop->have_posts() ) : $loop->the_post();
+                $bus_id = get_the_ID();
+                $first_image = get_field( 'bus_imagine' );
+                $images = get_field( 'gelerie_bus', $bus_id );
+        ?>
+                <div class="shadow-md rounded p-6 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg bg-gray-100">
+                    <div class="flex flex-wrap md:flex-nowrap">
+                        <div class="w-full md:w-1/3 mb-4 md:mb-0">
+                            <a href="<?php echo esc_url( $first_image ); ?>" class="block mb-4 popup-gallery-<?php echo $bus_id; ?>" title="<?php echo esc_attr( get_the_title() ); ?>">
+                                <img src="<?php echo esc_url( $first_image ); ?>" alt="" class="w-full rounded-lg">
+                            </a>
                         </div>
-                        <div class="form-rent-bus-container" id="<?php echo 'bus-' . get_the_ID() ?>">
-                            <div class="col-sm-12 col-md-4 bus-info-col">
-                                <div class="bus-info">
-                                    <div class="bus-info-title"><?php the_field( 'marca_bus' ) ?></div>
-                                    <div class="bus-model"><?php _e('Model')?> : <?php the_field( 'model_bus' ) ?></div>
-                                    <div class="availability">
-                                        <?php _e('Disponibilitate')?>: <?php the_field( 'disponibilitate' ) ?> <?php if( get_field('disponibilitate') == 1) {echo __('masina');}else{ echo __('masini');}?>
-                                    </div>
-                                    <div class="seats">
-                                        <span><?php _e('Numar locuri')?>: <?php the_field( 'nr_locuri' ) ?></span>
-                                    </div>
+                        <div class="w-full md:w-2/3 text-center md:text-left md:pl-4">
+                            <div class="flex justify-between items-start">
+                                <div>
+                                    <h2 class="text-3xl font-bold mb-2"><?php echo get_the_title(); ?></h2>
+                                </div>
+                                <div class="text-right">
+                                    <span class="block">
+                                        <strong class="text-3xl"><?php the_field( 'nr_locuri' ); ?> <span class="text-xl"><?php _e('locuri')?></span></strong>
+                                    </span>
+                                    <div class="text-gray-600 mb-2"><?php _e('Disponibilitate')?>: <?php the_field( 'disponibilitate' ); ?> <?php echo (get_field('disponibilitate') == 1) ? __('masina') : __('masini'); ?></div>
                                 </div>
                             </div>
-                            <div class="container-form-bus">
-                                <?php if(get_locale() == 'ro_RO'): ?>
+                            <span class="text-gray-600 block mb-2 font-bold"><?php _e('Dotari interioare')?></span>
+                            <div class="flex flex-wrap pl-4 mb-4">
+                                <?php
+                                if ( have_rows( 'dotari_interioare' ) ) :
+                                    while ( have_rows( 'dotari_interioare' ) ) : the_row();
+                                ?>
+                                        <div class="w-full md:w-1/2 mb-2"><?php the_sub_field( 'dotare' ); ?></div>
+                                <?php
+                                    endwhile;
+                                endif;
+                                ?>
+                            </div>
+                            <a href="#" class="button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full inline-block" data-bus="<?php echo 'bus-' . $bus_id; ?>"><span style="color: #FFFFFF" class="close">Solicita oferta</span></a>
+                        </div>
+                    </div>
+                    <!-- Container pentru formularul de închiriere -->
+                    <div class="form-rent-bus-container bg-gray-100 p-4 rounded-lg mb-4 hidden" id="<?php echo 'bus-' . $bus_id; ?>">
+                        <div class="flex flex-wrap">
+                            <div class="w-full">
+                                <div class="container-form-bus bg-white p-4 rounded-lg mb-4">
                                     <?php echo do_shortcode( '[contact-form-7 id="1306" title="Formular autocare"]' ); ?>
-                                <?php else: ?>
-                                    <?php echo do_shortcode( '[contact-form-7 id="1628" title="Bus Rent Form"]' ); ?>
-                                <?php endif; ?>
+                                </div>
                             </div>
                         </div>
                     </div>
-                <?php endwhile; ?>
-            <?php endif;
-            wp_reset_postdata(); ?>
-        </div>
-    </div>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-10 col-sm-offset-1 homepage-info-text">
-                <?php the_field( 'text_informativ_pagina_principala' ) ?>
-            </div>
-        </div>
+                    <!-- Magnific Popup pentru galerie imagini -->
+                    <div class="hidden">
+                        <?php foreach ( $images as $index => $image ) : ?>
+                            <a href="<?php echo esc_url( $image['url'] ); ?>" class="popup-gallery-<?php echo $bus_id; ?>" title="<?php echo esc_attr( $image['alt'] ); ?>"></a>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+        <?php
+            endwhile;
+        endif;
+        wp_reset_postdata();
+        ?>
     </div>
 </div>
 
 <script>
-document.addEventListener('DOMContentLoaded', function() {
-    var buttons = document.querySelectorAll('.seats-availability .button');
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function(event) {
-            event.preventDefault();
-            var busId = button.getAttribute('data-bus');
-            var formContainer = document.getElementById(busId);
-            
-            // Toggle class 'active' on form container
-            formContainer.classList.toggle('active');
-            
-            // Update button text based on active state
-            var buttonText = button.querySelector('.close');
-            if (formContainer.classList.contains('active')) {
-                buttonText.textContent = 'Inchide solicitarea';
-            } else {
-                buttonText.textContent = 'Solicita oferta';
-            }
-        });
+jQuery(document).ready(function($) {
+    // Initialize Magnific Popup for each bus gallery
+    <?php
+    $loop = new WP_Query( $args );
+    if ( $loop->have_posts() ) :
+        while ( $loop->have_posts() ) : $loop->the_post();
+            $bus_id = get_the_ID();
+    ?>
+    $('.popup-gallery-<?php echo $bus_id; ?>').magnificPopup({
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+    });
+    <?php
+        endwhile;
+    endif;
+    wp_reset_postdata();
+    ?>
+
+    // Toggle form visibility
+    $('.button').on('click', function(e) {
+        e.preventDefault();
+        var busId = $(this).data('bus');
+        $('#' + busId).toggleClass('hidden');
+        var buttonText = $(this).find('.close');
+        if (buttonText.text() === 'Solicita oferta') {
+            buttonText.text('Inchide solicitarea');
+        } else {
+            buttonText.text('Solicita oferta');
+        }
     });
 });
+
 </script>
 
 <?php get_footer(); ?>
+
